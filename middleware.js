1fb94de458fdfeac8837a96754585281ea54ec0c
Prestonberry <Prestonberryy@gmail.com>
2026-06-14 06:23:55 -0400

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,17 +4,20 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/pricing',
   '/affiliates',
-  '/api/webhook',
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api/webhook',
 ]);
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect();
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    auth().protect();
   }
 });
 
 export const config = {
-  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };

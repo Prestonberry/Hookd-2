bda051fba29b01311a8c0daec579dc12003e9e8c
Prestonberry <Prestonberryy@gmail.com>
2026-06-29 00:32:56 -0400

    Change auth protection to async in middleware

--- middleware.js
+++ middleware.js
@@ -9,9 +9,9 @@ const isPublicRoute = createRouteMatcher([
   '/api/webhook(.*)',
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
